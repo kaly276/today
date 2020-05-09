edab924d01fdf3018697274d0da79102cf2f4c13
Duy <duy257live@gmail.com>
2020-05-09 02:09:19 -0700

    test

--- todayjournal/frontend/src/components/accounts/Account.js
+++ todayjournal/frontend/src/components/accounts/Account.js
@@ -11,7 +11,7 @@ export class Account extends Component {
   
     render() {
         if (!this.props.isAuthenticated) {
-            return <Redirect to="/" />;
+            return <Redirect to='/' />
         }
 
         return (
